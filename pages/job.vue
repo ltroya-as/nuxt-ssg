7c3ad0b640f792f01380bf707202ad84e130999a
ltroya-as <luis.troya@allansolutions.com>
2021-07-12 14:55:46 -0500

    Add static content example

---
+++ pages/job.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.slug }}</p>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  data() {
+    return {
+      job: {},
+    }
+  },
+  async fetch() {
+    const response = await this.$apollo.query({
+      query: gql`
+        query {
+          job(
+            input: {
+              jobSlug: "senior-fullstack-engineer-platform"
+              companySlug: "segment"
+            }
+          ) {
+            title
+            slug
+            description
+          }
+        }
+      `,
+    })
+
+    this.job = response.data.job
+  },
+}
+</script>
