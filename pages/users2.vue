227956afc8a85fce462609fffd22904bd1bb36db
ltroya-as <luis.troya@allansolutions.com>
2021-07-12 11:31:24 -0500

    Add asyncData example

---
+++ pages/users2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mx-auto my-10 content-container">
+    <div v-for="user in users" :key="user.id" class="mb-10">
+      <h1 class="text-4xl font-bold">{{ user.name.first }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.get('https://randomuser.me/api?results=20')
+    return { users: response.data.results }
+  },
+}
+</script>
